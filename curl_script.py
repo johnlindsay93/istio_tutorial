af295f3fa9279ab5ddca73816525e939795a722a
johnlindsay93 <lindsayjohn1993@gmail.com>
2021-05-08 17:54:50 +0100

    Fixing minor bugs with yaml files

--- curl_script.py
+++ curl_script.py
@@ -1,10 +1,10 @@
 import requests
 import time
 
-BASE = "http://20.191.46.182:5000/"
+BASE = "http://20.67.180.192:5000/"
 
 while True:
-    response = requests.get(BASE + "helloworld")
+    response = requests.get(BASE + "services1")
     print(response.json())
     time.sleep(3)
 
